9d783157440b4b599ef592be2eaa261e5b1ae0a5
Gaurav Massand <gauravm636@gmail.com>
2020-05-16 20:01:03 +0530

    Initial Setup

---
+++ src/test/java/com/internet/speed/internet/speed/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internet.speed.internet.speed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
